767b80feb47de507a8d40e0d59e85c39a7fbdcf6
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-12-17 22:01:57 -0300

    update data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CardsModule } from '../../components/cards/cards.module';
 import { MenuModule } from '../../components/menu/menu.module';
+import { dataFake } from '../../data/placeholderData';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuModule, CardsModule],
+  imports: [MenuModule, CardsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss','./home.component.responsive.scss']
 })
 export class HomeComponent {
-
+  bigNews = dataFake[0];
+  news = dataFake.filter(data=> data.id!=='1')
 }
